d147d143f73bb9d43347cf59f5474391a849f45f
Glen Knight <glenak1911@gmail.com>
2018-06-10 00:50:43 -0400

    correct mention regex

--- murphybot.py
+++ murphybot.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import os
 import time
 import re
@@ -9,16 +8,39 @@ murphybot_id = None
 
 RTM_READ_DELAY = 1
 EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@|[WU].+?)(.*)"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+def parse_bot_commands(slack_events):
+   for event in slack_events:
+     if event["type"] == "message" and not "subtype" in event:
+       user_id, message = parse_direct_mention(event["text"])
+       if user_id == murphybot_id:
+         return message, event["channel"]
+   return None, None
+
+def parse_direct_mention(message_text):
+    matches = re.search(MENTION_REGEX, message_text)
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel):
+    default_response = "Not sure what you mean. Try *{}*".format(EXAMPLE_COMMAND)
+    response = None
+    if command.startswith(EXAMPLE_COMMAND):
+      response = "Sure...write some more code then I can do that!"
+    
+    slack_client.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text=response or default_response)
 
 if __name__=="__main__":
-  if slack_client.rtm_connect(with_team_state=False):
-    print("Murphybot connected and running!")
-    murphybot_id = slack_client.api_call("auth.test")["user_id"]
-  while True:
-    command, channel = parse_bot_commands(slack_client.rtm_read())
-    if command:
-      handle_command(command, channel)
-      time.sleep(RTM_READ_DELAY)
+    if slack_client.rtm_connect(with_team_state=False):
+      print("Murphybot connected and running!")
+      murphybot_id = slack_client.api_call("auth.test")["user_id"]
+      while True:
+        command, channel = parse_bot_commands(slack_client.rtm_read())
+        if command:
+          handle_command(command, channel)
+        time.sleep(RTM_READ_DELAY)
     else:
       print("Connection Failed. Exception traceback printed above.")
