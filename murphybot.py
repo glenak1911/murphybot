1942fb2bb5d5528eb685221da80b5263c0235b9e
Glen Knight <glenknight@knight-vision-v2.local>
2018-11-04 01:01:57 -0400

    update murphybot to a lambda function

--- murphybot.py
+++ murphybot.py
@@ -1,52 +1,63 @@
 import os
-import time
-import re
-from pymongo import MongoClient
 import json
-from slackclient import SlackClient
+import requests
+import pymysql.cursors
 
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-murphybot_id = None
+def handler(event, context):
+    # Grab webhook url
+    webhook_url = os.environ['WEBHOOK_URL']
 
-RTM_READ_DELAY = 1
-EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-pipeline = [{"$sample": { "size":1 }}]
+    slack_acknowledgement = requests.post(
+      webhook_url, 
+      data={"text":""},
+      headers={'Content-Type': 'application/json'}
+    )
 
-def parse_bot_commands(slack_events):
-   for event in slack_events:
-     if event["type"] == "message" and not "subtype" in event:
-       user_id, message = parse_direct_mention(event["text"])
-       if user_id == murphybot_id:
-         return message, event["channel"]
-   return None, None
+    connection = connectToDb()
 
-def parse_direct_mention(message_text):
-    matches = re.search(MENTION_REGEX, message_text)
-    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
-
-def handle_command(command, channel):
-    client = MongoClient()
-    db = client.murphyslaws
-    collection = db.laws
-    law = collection.aggregate(pipeline)
-    law = list(law)
-    law = law[0][u'law']
-    response = json.dumps(law)
+    result = getMessage(connection)
     
-    slack_client.api_call(
-      "chat.postMessage",
-      channel=channel,
-      text=response)
+    print("result: {}".format(result))
 
-if __name__=="__main__":
-    if slack_client.rtm_connect(with_team_state=False):
-      print("Murphybot connected and running!")
-      murphybot_id = slack_client.api_call("auth.test")["user_id"]
-      while True:
-        command, channel = parse_bot_commands(slack_client.rtm_read())
-        if command:
-          handle_command(command, channel)
-        time.sleep(RTM_READ_DELAY)
-    else:
-      print("Connection Failed. Exception traceback printed above.")
+    law = result["Title"]
+
+    print("sending {} data to: {}".format(law, webhook_url))
+
+    response_data = json.dumps({"text":"{}".format(law)})
+
+    postToSlack(response_data, webhook_url)
+
+    return { "isBase64Encoded": True, "statusCode": 200, "headers": { }, "body": "" }
+
+def connectToDb():
+    connection = pymysql.connect(host=os.environ['DB_ENDPOINT'],
+                                 user=os.environ['DB_USER'],
+                                 password=os.environ['DB_PASSWORD'],
+                                 db='murphyslaws',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    
+    print("connection info: {}".format(connection))
+
+    return connection
+
+def getMessage(db_connection):
+    try:
+        with db_connection.cursor() as cursor:
+            sql = "SELECT * FROM laws ORDER BY RAND() LIMIT 1"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+    finally:
+        db_connection.close()
+
+    return result
+
+def postToSlack(response_data, webhook_url):
+    slack_message = requests.post(
+      webhook_url, 
+      data=response_data,
+      headers={'Content-Type': 'application/json'}
+    )
+
+    print(slack_message.statusCode)
